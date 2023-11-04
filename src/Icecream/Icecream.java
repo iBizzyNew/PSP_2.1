d70f18c20ba125a812802435c714fe318996ac95
iBizzyNew <jaroslavsharaphanovich@gmail.com>
2023-11-04 12:50:17 +0300

    secondCommit

--- src/Icecream/Icecream.java
+++ src/Icecream/Icecream.java
@@ -1,2 +1,90 @@
-package Icecream;public class Icecream {
+package Icecream;
+/**
+ * Состав класса Icecream (мороженое): название, наличие шоколада, процент жирности. Подсчитать средний процент жирности и количество мороженых с шоколадом.
+ */
+
+public class Icecream {
+    private String name;
+    private boolean hasChocolate;
+    private double fatPercentage;
+
+
+    public Icecream() { // Конструктор по умолчанию
+        name = "";
+        hasChocolate = false;
+        fatPercentage = 0.0;
+    }
+
+    public Icecream(Icecream otherIcecream) {    // Конструктор копирования
+        this.name = otherIcecream.name;
+        this.hasChocolate = otherIcecream.hasChocolate;
+        this.fatPercentage = otherIcecream.fatPercentage;
+    }
+
+    public Icecream(String name, boolean hasChocolate, double fatPercentage) { // Конструктор со всеми параметрами
+        this.name = name;
+        this.hasChocolate = hasChocolate;
+        this.fatPercentage = fatPercentage;
+    }
+
+    // Геттеры (методы для получения значений полей)
+    public String getName() {
+        return name;
+    }
+
+    public boolean hasChocolate() {
+        return hasChocolate;
+    }
+
+    public double getFatPercentage() {
+        return fatPercentage;
+    }
+
+    // Сеттеры (методы для установки значений полей)
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHasChocolate(boolean hasChocolate) {
+        this.hasChocolate = hasChocolate;
+    }
+
+    public void setFatPercentage(double fatPercentage) {
+        this.fatPercentage = fatPercentage;
+    }
+
+    // Метод для вывода информации
+    public void printInfo() {
+        System.out.println("Мороженное: " + name);
+        System.out.println("Наличие шоколада: " + hasChocolate);
+        System.out.println("Процент жирности: " + fatPercentage);
+    }
+
+    // Статический метод для подсчета среднего процента жирности
+    public static double calculateAverageFatPercentage(Icecream[] icecreams) { // В качестве параметра передаем массив мороженых (Icecream[])
+        if (icecreams.length == 0) {
+            return 0.0;
+        }
+
+        double sum = 0;
+        for (Icecream icecream : icecreams) {
+            sum += icecream.getFatPercentage();
+        }
+
+        return sum / icecreams.length; // Возвращаем среднее значение процента жирности
+    }
+
+    // Статический метод для подсчета количества мороженых с шоколадом
+    public static int countChocolateIcecreams(Icecream[] icecreams) { //
+        int count = 0;
+        for (Icecream icecream : icecreams) { //цикл for each(Пербираем icecreams, обращаясь к каждому элементу как к icecream)
+            if (icecream.hasChocolate()) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
+
+
+
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,23 @@
+package main;
+import Icecream.Icecream;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Icecream[] icecreams = new Icecream[3]; // Создание массива объектов класса Icecream
+        icecreams[0] = new Icecream("Ванильный мишка", false, 5.3);
+        icecreams[1] = new Icecream("Шоколадный заяц", true, 8.5);
+        icecreams[2] = new Icecream("Клубничная бомба", false, 4.2);
+        //icecreams[3] = new Icecream(icecreams[1]);
+        //icecreams[4] = new Icecream();
+
+        for (Icecream icecream : icecreams) {
+            icecream.printInfo();
+            System.out.println();
+        }
+
+        double averageFatPercentage = Icecream.calculateAverageFatPercentage(icecreams); // Вызов статического метода
+        int chocolateIcecreamCount = Icecream.countChocolateIcecreams(icecreams); // Вызов статического метода
+
+        System.out.println("Средний процент жирности захаваного мороженого: " + averageFatPercentage);
+        System.out.println("Кол-во чоколатных мороженых: " + chocolateIcecreamCount);
     }
 }
